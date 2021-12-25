b60303f17f6adb6f93f758b5e5d0236bc5c199f8
ShuvoKaisar <62845862+ShuvoKaisar@users.noreply.github.com>
2021-12-25 20:58:27 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".animation-text", {
+	strings: [
+		" Website Designer",
+		" Developer",
+		" Graphic Designer"
+	],
+		typeSpeed: 80,
+		backSpeed: 50,
+		delay: 50,
+		loop: true
+});
+
+
+
